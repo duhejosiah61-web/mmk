a4bde278c97c33e243f68457662210bc0aa47ef7
sorak <duhejosiah61@gmail.com>
2026-08-18 00:29:34 +0800

    Improve safety for anonymous_token file read
    Added safety checks for reading anonymous_token to prevent crashes when the file does not exist.

--- util/request.js
+++ util/request.js
@@ -10,12 +10,12 @@ const path = require('path')
 const tmpPath = require('os').tmpdir()
 const { cookieToJson, cookieObjToString, toBoolean } = require('./index')
 
-// ✅ 安全读取 anonymous_token，文件不存在时不崩溃
+// 修复点：添加安全读取判断，避免文件不存在时抛出 ENOENT 导致云函数崩溃
 let anonymous_token = ''
 try {
-  const tokenFile = path.resolve(tmpPath, './anonymous_token')
-  if (fs.existsSync(tokenFile)) {
-    anonymous_token = fs.readFileSync(tokenFile, 'utf-8')
+  const tokenFilePath = path.resolve(tmpPath, './anonymous_token')
+  if (fs.existsSync(tokenFilePath)) {
+    anonymous_token = fs.readFileSync(tokenFilePath, 'utf-8')
   }
 } catch (e) {
   anonymous_token = ''
@@ -85,10 +85,12 @@ const createRequest = (uri, data, options) => {
   return new Promise((resolve, reject) => {
     let headers = options.headers || {}
     let ip = options.realIP || options.ip || ''
+    // console.log(ip)
     if (ip) {
       headers['X-Real-IP'] = ip
       headers['X-Forwarded-For'] = ip
     }
+    // headers['X-Real-IP'] = '118.88.88.88'
 
     let cookie = options.cookie || {}
     if (typeof cookie === 'string') {
@@ -100,6 +102,7 @@ const createRequest = (uri, data, options) => {
       cookie = {
         ...cookie,
         __remember_me: 'true',
+        // NMTID: CryptoJS.lib.WordArray.random(16).toString(),
         ntes_kaola_ad: '1',
         _ntes_nuid: cookie._ntes_nuid || _ntes_nuid,
         _ntes_nnid:
@@ -117,6 +120,7 @@ const createRequest = (uri, data, options) => {
         cookie['NMTID'] = CryptoJS.lib.WordArray.random(16).toString()
       }
       if (!cookie.MUSIC_U) {
+        // 游客
         cookie.MUSIC_A = cookie.MUSIC_A || anonymous_token
       }
       headers['Cookie'] = cookieObjToString(cookie)
@@ -128,6 +132,7 @@ const createRequest = (uri, data, options) => {
       csrfToken = cookie['__csrf'] || ''
 
     if (crypto === '') {
+      // 加密方式为空，以配置文件的加密方式为准
       if (APP_CONF.encrypt) {
         crypto = 'eapi'
       } else {
@@ -135,6 +140,7 @@ const createRequest = (uri, data, options) => {
       }
     }
 
+    // 根据加密方式加密请求数据；目前任意uri都支持四种加密方式
     switch (crypto) {
       case 'weapi':
         headers['Referer'] = APP_CONF.domain
@@ -157,17 +163,18 @@ const createRequest = (uri, data, options) => {
 
       case 'eapi':
       case 'api':
+        // 两种加密方式，都应生成客户端的cookie
         const header = {
-          osver: cookie.osver,
+          osver: cookie.osver, //系统版本
           deviceId: cookie.deviceId,
-          os: cookie.os,
-          appver: cookie.appver,
-          versioncode: cookie.versioncode || '140',
-          mobilename: cookie.mobilename || '',
+          os: cookie.os, //系统类型
+          appver: cookie.appver, // app版本
+          versioncode: cookie.versioncode || '140', //版本号
+          mobilename: cookie.mobilename || '', //设备model
           buildver: cookie.buildver || Date.now().toString().substr(0, 10),
-          resolution: cookie.resolution || '1920x1080',
+          resolution: cookie.resolution || '1920x1080', //设备分辨率
           __csrf: csrfToken,
-          channel: cookie.channel,
+          channel: cookie.channel, //下载渠道
           requestId: `${Date.now()}_${Math.floor(Math.random() * 1000)
             .toString()
             .padStart(4, '0')}`,
@@ -183,27 +190,31 @@ const createRequest = (uri, data, options) => {
         headers['User-Agent'] = options.ua || chooseUserAgent('api', 'iphone')
 
         if (crypto === 'eapi') {
+          // 使用eapi加密
           data.header = header
           data.e_r =
             options.e_r != undefined
               ? options.e_r
               : data.e_r != undefined
               ? data.e_r
-              : APP_CONF.encryptResponse
+              : APP_CONF.encryptResponse // 用于加密接口返回值
           data.e_r = toBoolean(data.e_r)
           encryptData = encrypt.eapi(uri, data)
           url = APP_CONF.apiDomain + '/eapi/' + uri.substr(5)
         } else if (crypto === 'api') {
+          // 不使用任何加密
           url = APP_CONF.apiDomain + uri
           encryptData = data
         }
         break
 
       default:
+        // 未知的加密方式
         console.log('[ERR]', 'Unknown Crypto:', crypto)
         break
     }
     const answer = { status: 500, body: {}, cookie: [] }
+    // console.log(headers, 'headers')
     let settings = {
       method: 'POST',
       url: url,
@@ -250,7 +261,6 @@ const createRequest = (uri, data, options) => {
     } else {
       settings.proxy = false
     }
-
     axios(settings)
       .then((res) => {
         const body = res.data
@@ -259,6 +269,7 @@ const createRequest = (uri, data, options) => {
         )
         try {
           if (data.e_r) {
+            // eapi接口返回值被加密，需要解密
             answer.body = encrypt.eapiResDecrypt(
               body.toString('hex').toUpperCase(),
             )
@@ -276,9 +287,12 @@ const createRequest = (uri, data, options) => {
             [201, 302, 400, 502, 800, 801, 802, 803].indexOf(answer.body.code) >
             -1
           ) {
+            // 特殊状态码
             answer.status = 200
           }
         } catch (e) {
+          // console.log(e)
+          // can't decrypt and can't parse directly
           answer.body = body
           answer.status = res.status
         }
