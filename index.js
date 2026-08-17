f7c286781a966a9b62c5572435db1f03cd0d9292
sorak <duhejosiah61@gmail.com>
2026-08-18 00:10:38 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('./app.js')
+const { serveNcmApi } = require('./server')
+
+let appPromise = null
+
+module.exports = async (req, res) => {
+  if (!appPromise) {
+    appPromise = serveNcmApi({})
+  }
+  const app = await appPromise
+  return app(req, res)
+}
